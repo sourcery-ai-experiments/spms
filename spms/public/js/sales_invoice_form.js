1af3fb4d44a5a372de33dd49a678a1175d438c94
osama1998H <osamamuhammed555@gmail.com>
2022-09-15 11:30:43 +0300

    add custom fields

--- spms/public/js/sales_invoice_form.js
+++ spms/public/js/sales_invoice_form.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('Sales Team', {
-	sales_person:function(frm) {
+	refresh:function(frm) {
         for(var i=0; i<frm.doc.sales_team.length;i++){
             frm.doc.sales_team[i].incentives = frm.doc.net_total * (frm.doc.sales_team[i].commission_rate / 100);
         }
