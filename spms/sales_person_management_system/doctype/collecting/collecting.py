22a5814bf1134a0c21904485ed379cd9265b8802
osama1998H <osamamuhammed555@gmail.com>
2022-09-21 16:09:13 +0300

    some fixes

--- spms/sales_person_management_system/doctype/collecting/collecting.py
+++ spms/sales_person_management_system/doctype/collecting/collecting.py
@@ -19,7 +19,67 @@ class Collecting(Document):
 
 		# find if Doctor and Item is Match with Goal
 		for i in collect_goal_doc.customer_collects_goal:
-			if(i.customer == self.customer_name):
-				i.verified_collects = i.verified_collects + self.amount_of_money
+			if(i.customer == self.customer):
+				i.verified_collects = i.verified_collects + self.amount
 				break
+
+		# Calculate total amount for Verified Collects
+		total = 0
+		for i in collect_goal_doc.customer_collects_goal:
+			total = total + i.verified_collects
+
+		# set total value for Total Collected in Collected Goal
+		collect_goal_doc.total_collected = frappe.utils.flt(total)
+
+		total_incentives = 0
+		# calculate the incentives
+		if len(collect_goal_doc.commissions_range)!=0:
+			target_percentge = (collect_goal_doc.total_collected / collect_goal_doc.total_targets) * 100
+			for i in collect_goal_doc.commissions_range:
+				if target_percentge >= i.from_ and target_percentge <= i.to_:
+					total_incentives = (i.commission/100) * collect_goal_doc.total_collected
+					break
+
+		# set total value for Total Collected in Collected Goal
+		collect_goal_doc.incentives = frappe.utils.flt(total_incentives)
+		collect_goal_doc.save()
+
+	
+	def on_cancel(self) -> None:
+		# Get name of Collects Goal Document Depend on Sales Person and Date
+		collect_goal_name = frappe.db.get_value('Collects Goal', {
+			'sales_person': self.visited_by,
+			'company':self.company,
+			'from':['<=', self.date],
+			'to':['>=', self.date]
+		}, ['name'], as_dict=1)
+
+		# Get objects for Specific Collects Goal
+		collect_goal_doc = frappe.get_doc('Collects Goal', collect_goal_name)
+
+		# find if Doctor and Item is Match with Goal
+		for i in collect_goal_doc.customer_collects_goal:
+			if(i.customer == self.customer):
+				i.verified_collects = i.verified_collects - self.amount
+				break
+
+		# Calculate total amount for Verified Collects
+		total = 0
+		for i in collect_goal_doc.customer_collects_goal:
+			total = total + i.verified_collects
+
+		# set total value for Total Collected in Collected Goal
+		collect_goal_doc.total_collected = frappe.utils.flt(total)
+
+		total_incentives = 0
+		# calculate the incentives
+		if len(collect_goal_doc.commissions_range)!=0:
+			target_percentge = (collect_goal_doc.total_collected / collect_goal_doc.total_targets) * 100
+			for i in collect_goal_doc.commissions_range:
+				if target_percentge >= i.from_ and target_percentge <= i.to_:
+					total_incentives = (i.commission/100) * collect_goal_doc.total_collected
+					break
+
+		# set total value for Total Collected in Collected Goal
+		collect_goal_doc.incentives = frappe.utils.flt(total_incentives)
 		collect_goal_doc.save()
--- spms/sales_person_management_system/doctype/collects_goal/collects_goal.js
+++ spms/sales_person_management_system/doctype/collects_goal/collects_goal.js
@@ -3,13 +3,66 @@
 
 
 frappe.ui.form.on('Collects Goal', {
-	"to":function(frm) {
-		if(frm.doc.to < frm.doc.from){
-	        frappe.msgprint("Please, Select Valid Period for Target)")
-	    }
-	    
-	    // find diff between in and out date
-	    let diff_days = frappe.datetime.get_day_diff( frm.doc.to, frm.doc.from );
-	    frm.set_value("number_of_days",diff_days);
+	"to": function (frm) {
+		if (frm.doc.to < frm.doc.from) {
+			frappe.msgprint("Please, Select Valid Period for Target)")
+		}
+
+		// find diff between in and out date
+		let diff_days = frappe.datetime.get_day_diff(frm.doc.to, frm.doc.from);
+		frm.set_value("number_of_days", diff_days);
 	}
 })
+
+// Trigger When Add New Row To Table
+frappe.ui.form.on('Customer Collects Goal', {
+	customer_collects_goal_add: function (frm, cdt, cdn) {
+		var total = 0;
+		$.each(frm.doc.customer_collects_goal || [], function (i, d) {
+			total += flt(d.amount_of_money);
+		});
+		frm.set_value("total_targets", total);
+		frm.refresh();
+	}
+});
+
+// Trigger When Remove Row Table
+frappe.ui.form.on('Customer Collects Goal', {
+	customer_collects_goal_remove: function (frm, cdt, cdn) {
+		var total = 0;
+		$.each(frm.doc.customer_collects_goal || [], function (i, d) {
+			total += flt(d.amount_of_money);
+		});
+		frm.set_value("total_targets", total);
+		frm.refresh();
+	}
+});
+
+// Trigger On Every Event On [amount_of_money] Field
+frappe.ui.form.on('Customer Collects Goal', {
+	amount_of_money: function (frm, cdt, cdn) {
+		var total = 0;
+		$.each(frm.doc.customer_collects_goal || [], function (i, d) {
+			total += flt(d.amount_of_money);
+		});
+		frm.set_value("total_targets", total);
+		frm.refresh();
+	}
+});
+
+frappe.ui.form.on('Commissions range', {
+	form_render: function (frm, cdt, cdn) {
+		$.each(frm.doc.commissions_range || [], function (i, d) {
+			if (d.from_ > d.to_) {
+				msgprint('Please, (From) Percentage is Greater than (To) Percentage');
+				validated = false;
+			} else if (d.to_ < d.from_) {
+				msgprint('Please, (To) Percentage is Less than (From) Percentage');
+				validated = false;
+			} else if (d.to_ == d.from_ && d.to_ != 0 && d.from_ != 0) {
+				msgprint('Please, (To) Percentage is Equal to (From) Percentage');
+				validated = false;
+			}
+		});
+	}
+});
