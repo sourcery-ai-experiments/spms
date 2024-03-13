a88d5d547bed9319d8bfc413ccfd9001070d3567
Ahmed Ghazi <ahmedgzi07@gmail.com>
2024-03-13 13:58:09 +0300

    set the full name

--- spms/sales_person_management_system/doctype/client/client.py
+++ spms/sales_person_management_system/doctype/client/client.py
@@ -6,5 +6,4 @@ from frappe.model.document import Document
 
 
 class Client(Document):
-    def validate(self):
-        self.fullname = self.first_name + " " + self.mid_name + " " +self.last_name + " "
+    pass
