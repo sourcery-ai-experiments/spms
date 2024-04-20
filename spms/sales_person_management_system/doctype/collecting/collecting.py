fc1c1e64ec28be3b914a074a19d2788d23505dcd
Ahmed Ghazi Samari <67123870+AhmedSama@users.noreply.github.com>
2024-04-20 14:45:06 +0300

    fix indentation again

--- spms/sales_person_management_system/doctype/collecting/collecting.py
+++ spms/sales_person_management_system/doctype/collecting/collecting.py
@@ -39,5 +39,5 @@ class Collecting(WebsiteGenerator):
 				site_name=site_name, route_name=self.route)
 			self.image = image_path
 		# Calculate outstanding amount for each invoice
-			for invoice in self.invoices:
-				invoice.out_standing_amount = invoice.out_standing_amount - invoice.allocated_amount
+		for invoice in self.invoices:
+			invoice.out_standing_amount = invoice.out_standing_amount - invoice.allocated_amount
