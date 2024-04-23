28488c75be8b27b5be80898bbb4f61b610e332da
osama1998H <osamamuhammed555@gmail.com>
2024-04-23 14:00:48 +0300

    finish

--- spms/public/js/item.js
+++ spms/public/js/item.js
@@ -14,7 +14,7 @@ frappe.ui.form.on('Item', {
             // Create HTML content for barcode buttons
             var dialogContent = '<div id="barcode-buttons">';
             barcodes.forEach(function (barcode) {
-                dialogContent += '<button class="barcode-button" data-barcode="' + barcode.barcode + '">' + barcode.barcode + '</button>';
+                dialogContent += '<button class="barcode-button btn btn-primary" data-barcode="' + barcode.barcode + '">' + barcode.barcode + '</button>';
             });
             dialogContent += '</div>';
 
@@ -32,42 +32,42 @@ frappe.ui.form.on('Item', {
                 primary_action_label: 'Print',
                 primary_action: function () {
                     // Get the selected barcode
-                    var selectedBarcode = dialog.get_value('barcode_buttons');
-                    console.log(selectedBarcode)
+                    var selectedButton = dialog.get_field('barcode_buttons').$wrapper.find('.barcode-button.active');
+                    var selectedBarcode = selectedButton ? selectedButton.attr('data-barcode') : null;
 
-                    // Get the current site URL
-                    frappe.call({
-                        method: 'spms.methods.utils.get_base_url',
-                        callback: function (response) {
-                            var baseUrl = response.message;
+                    if (selectedBarcode) {
+                        // Get the current site URL
+                        frappe.call({
+                            method: 'spms.methods.utils.get_base_url',
+                            callback: function (response) {
+                                var baseUrl = response.message;
 
-                            // Generate the URL parameters
-                            var doctype = 'Item';
-                            var name = frm.doc.name;
-                            var format = 'Barcode 35mm x 15mm';
-                            var no_letterhead = '1';
-                            var letterhead = 'No Letterhead';
-                            var settings = '{}';
-                            var _lang = 'en';
-                            var barcode = selectedBarcode;
+                                // Generate the URL parameters
+                                var doctype = 'Item';
+                                var name = frm.doc.name;
+                                var format = 'Barcode 35mm x 15mm';
+                                var no_letterhead = '1';
+                                var letterhead = 'No Letterhead';
+                                var settings = '{}';
+                                var _lang = 'en';
+                                var barcode = selectedBarcode;
 
-                            // Generate the complete URL
-                            var url = baseUrl + '/printview?' +
-                                'doctype=' + encodeURIComponent(doctype) +
-                                '&name=' + encodeURIComponent(name) +
-                                '&format=' + encodeURIComponent(format) +
-                                '&no_letterhead=' + encodeURIComponent(no_letterhead) +
-                                '&letterhead=' + encodeURIComponent(letterhead) +
-                                '&settings=' + encodeURIComponent(settings) +
-                                '&_lang=' + encodeURIComponent(_lang) +
-                                '&barcode=' + encodeURIComponent(barcode);
+                                // Generate the complete URL
+                                var url = baseUrl + '/printview?' +
+                                    'doctype=' + encodeURIComponent(doctype) +
+                                    '&name=' + encodeURIComponent(name) +
+                                    '&format=' + encodeURIComponent(format) +
+                                    '&no_letterhead=' + encodeURIComponent(no_letterhead) +
+                                    '&letterhead=' + encodeURIComponent(letterhead) +
+                                    '&settings=' + encodeURIComponent(settings) +
+                                    '&_lang=' + encodeURIComponent(_lang) +
+                                    '&barcode=' + encodeURIComponent(barcode);
 
-                            // Navigate to the generated URL
-                            // window.location.href = url;
-                            window.open(url, '_blank');
-                            console.log(url)
-                        }
-                    });
+                                // Open the URL in a new window
+                                window.open(url, '_blank');
+                            }
+                        });
+                    }
 
                     // Close the dialog
                     dialog.hide();
@@ -76,6 +76,19 @@ frappe.ui.form.on('Item', {
 
             // Show the dialog
             dialog.show();
+
+            // Add click event listener to barcode buttons
+            dialog.$wrapper.find('.barcode-button').on('click', function () {
+                // Remove active class from all buttons
+                dialog.$wrapper.find('.barcode-button').removeClass('active');
+                // Add active class to the clicked button
+                $(this).addClass('active');
+                // Add primary class to the clicked button
+                $(this).addClass('btn-primary');
+            });
+
+            // Add margin between barcode buttons
+            dialog.$wrapper.find('.barcode-button').css('margin-right', '5px');
         });
 
     }
